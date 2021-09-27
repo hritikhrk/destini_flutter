4abb1d8aa7696ae2025b7900bc929699b8d643f3
Hritik Kumar <hritik.kmr.111@gmail.com>
2021-09-27 19:02:54 +0530

    add story data

--- lib/story.dart
+++ lib/story.dart
@@ -3,9 +3,5 @@ class Story {
   String choice1 = "";
   String choice2 = "";
 
-  Story(String title, String c1, String c2) {
-    this.storyTitle = title;
-    this.choice1 = c1;
-    this.choice2 = c2;
-  }
+  Story({this.storyTitle = "", this.choice1 = "", this.choice2 = ""});
 }
